43cef16af71a2f3f8227567595ac88abbeeb6abc
Vadym Chumasov <u2107680007911543774e7r@Vadyms-MacBook-Air.local>
2023-09-09 20:46:06 +0300

    ebaniy rot

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -12,6 +12,21 @@ const iconTrade = document.getElementById("side-button-trade-icon");
 const iconAssets = document.getElementById("side-button-assets-icon");
 const iconAcc = document.getElementById("side-button-account-icon");
 
+
+// HELP LOGOUT
+const helpButton = document.getElementById("help-button")
+const helpIcon = document.getElementById('information-circle-icon')
+const logoutButton = document.getElementById("logout-button")
+const faqDiv = document.getElementById('faq')
+
+
+// NAV
+const newDealButton = document.getElementById("newDealButton")
+const ordersHistoryButton = document.getElementById("ordersHistoryButton")
+const newOrder = document.getElementById('newOrder')
+const tradeDiv = document.getElementById('trade-div')
+const tableOrders = document.getElementById('orders')
+
 // Add a click event listener to the button
 sideButtonAssets.addEventListener("click", function() {
   // Check if the element is currently hidden
@@ -32,12 +47,22 @@ sideButtonTrade.addEventListener("click", function() {
     accToToggle.setAttribute("style", "display: none;")
     tradeToToggle.removeAttribute("style");
     ordersToToggle.removeAttribute("style");
+    tradeDiv.style.display = 'flex'
 
     sideButtonAssets.classList.remove("active");
     sideButtonAcc.classList.remove("active");
     sideButtonTrade.classList.add("active");
     iconAcc.classList.remove("active");
     iconTrade.classList.add("active");
+
+    faqDiv.style.display = 'none'
+    helpButton.classList.remove('active')
+    helpIcon.classList.remove('active')
+
+    newDealButton.add('active')
+    ordersHistoryButton.add('active')
+    newDealButton.style.display = 'flex'
+    ordersHistoryButton.style.display = 'flex'
   }
 });
 
@@ -46,6 +71,7 @@ sideButtonAcc.addEventListener('click', function() {
     tradeToToggle.setAttribute("style", "display: none;")
     accToToggle.removeAttribute("style");
     ordersToToggle.setAttribute("style", "display: none;")
+    tradeDiv.style.display = 'none'
 
     sideButtonTrade.classList.remove("active");
     sideButtonAssets.classList.remove("active");
@@ -53,18 +79,86 @@ sideButtonAcc.addEventListener('click', function() {
     iconTrade.classList.remove("active");
     iconAssets.classList.remove("active");
     iconAcc.classList.add("active");
+
+    faqDiv.style.display = 'none'
+    helpButton.classList.remove('active')
+    helpIcon.classList.remove('active')
+
+    newDealButton.add('active')
+    ordersHistoryButton.add('active')
   }
 });
 
-// JavaScript code for handling the button click event
-document.getElementById("side-out-button").addEventListener("click", function() {
-  // Redirect to another.html
-  window.location.href = "login.html";
-});
+// NAV BUTTONS 
 
+newDealButton.addEventListener("click", function() {
+  if (!newDealButton.classList.contains('active')) {
+    newOrder.removeAttribute("style")
+    newDealButton.classList.add('active')
+    newDealButton.style.backgroundColor = '#1e222d'
+    newDealButton.style.border = '1px solid rgb(0, 255, 110)'
+    newDealButton.style.color = 'rgb(0, 255, 110)'
+    newDealButton.style.fontWeight = 'bold'
+  } else {
+    newOrder.style.display = 'none'
+    newDealButton.classList.remove('active')
+    newDealButton.removeAttribute('style')
+  }
+})
+
+ordersHistoryButton.addEventListener("click", function() {
+  if (!ordersHistoryButton.classList.contains('active')) {
+    ordersHistoryButton.classList.add("active")
+    ordersHistoryButton.style.backgroundColor = '#1e222d'
+    ordersHistoryButton.style.border = '1px solid rgb(0, 255, 110)'
+    ordersHistoryButton.style.color = 'rgb(0, 255, 110)'
+    ordersHistoryButton.style.fontWeight = 'bold'
+
+    tableOrders.style.maxHeight = '500px'
+    tradeToToggle.style.display = 'none'
+  } else {
+    ordersHistoryButton.classList.remove("active")
+    ordersHistoryButton.removeAttribute('style')
+
+    tableOrders.style.maxHeight = '154px'
+    tradeToToggle.style.display = 'flex'
+    sideButtonTrade.classList.add('active')
+    iconTrade.classList.add("active")
+  }
+})
 
 // MODAL DEPOSIT LOGIC 
 
-const openBtn = document.getElementById("deposit-button")
-const closeBtn = document.getElementById("modal-inner-close-button")
-const modal = document.getElementById("modal")
+
+// HELP + LOGOUT BUTTONS
+
+helpButton.addEventListener("click", function() {
+  if (!helpButton.classList.contains('active')){
+    sideButtonTrade.classList.remove("active")
+    sideButtonAssets.classList.remove("active")
+    sideButtonAcc.classList.remove("active")
+    iconTrade.classList.remove("active")
+    iconAssets.classList.remove("active")
+    iconAcc.classList.remove("active")
+    newDealButton.remove('active')
+    ordersHistoryButton.remove('active')
+    
+    tradeToToggle.style.display = 'none'
+    accToToggle.style.display = 'none'
+    assetsToToggle.style.display = 'none'
+    tableOrders.style.display = 'none'
+    tradeDiv.style.display = 'none'
+    
+    faqDiv.removeAttribute('style')
+    helpButton.classList.add('active')
+    helpIcon.classList.add('active')
+  } else {
+    faqDiv.style.display = 'none'
+    helpButton.classList.remove('active')
+    helpIcon.classList.remove('active')
+  }
+});
+
+logoutButton.addEventListener("click", function() {
+  window.location.href = "login.html";
+});
---
+++ src/components/test3.js
@@ -0,0 +1,5 @@
+// JavaScript code to handle button click event
+document.getElementById('btn').addEventListener('click', function() {
+  const el = document.getElementById('btn')
+  el.style.backgroundColor = 'red'
+});
