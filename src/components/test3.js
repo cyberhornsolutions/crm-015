b014c8fee04cccecffe0958fd8b5733dbf6c4928
Timur Chumasov <timurchumasov@Timurs-MacBook-Air.local>
2023-10-03 16:18:49 +0300

    del

--- src/components/test3.js
+++
@@ -1,5 +0,0 @@
-// JavaScript code to handle button click event
-document.getElementById('btn').addEventListener('click', function() {
-  const el = document.getElementById('btn')
-  el.style.backgroundColor = 'red'
-});
