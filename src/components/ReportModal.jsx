2c86d9c8c6100194057e599bf59777c6405c7132
Mubasher Rasheed <bitf12m001@gmail.com>
2024-01-24 18:32:36 +0500

    reports modal design refactor & add total profit & total deals

--- src/components/ReportModal.jsx
+++ src/components/ReportModal.jsx
@@ -1,9 +1,92 @@
 // Example: EditModal.js
-import React from "react";
-import { Button, Modal } from "react-bootstrap";
-import ReportTabs from "./ReportTabs";
+import { useEffect, useState } from "react";
+import { Modal } from "react-bootstrap";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+import DataTable from "react-data-table-component";
+import { depositColumns, generalColumns } from "../helper/Tablecolumns";
+import moment from "moment";
+import { useSelector } from "react-redux";
+import { getDepositsByUser } from "../helper/firebaseHelpers";
+import { fillArrayWithEmptyRows } from "../helper/helpers";
 
 const ReportModal = ({ onClose, show, userId }) => {
+  const orders = useSelector((state) =>
+    state.orders.filter(({ status }) => status != "Pending")
+  );
+  const [key, setKey] = useState("tradeOperations");
+  const [deposits, setDeposits] = useState([]);
+  const [showRecord, setShowRecord] = useState("all");
+
+  const totalProfit = orders.reduce((p, v) => p + v.profit, 0);
+
+  const customStyle = {
+    table: { style: { height: "70vh", backgroundColor: "#2f323d" } },
+  };
+
+  useEffect(() => {
+    if (!deposits.length) getDepositsByUser(userId, setDeposits);
+  }, []);
+  const today = moment();
+
+  let filteredOrders;
+  if (showRecord == "all") {
+    filteredOrders = orders;
+  } else if (showRecord === "today") {
+    const todayStart = today.startOf("day"); // Start of today
+    const dataCreatedToday = orders.filter((order) => {
+      return moment(order.createdAt).isSame(todayStart, "day");
+    });
+    filteredOrders = dataCreatedToday;
+  } else if (showRecord === "lastWeek") {
+    const sevenDaysAgo = today.subtract(7, "days");
+    const dataCreatedToday = orders.filter((order) => {
+      return moment(order.createdAt).isSameOrAfter(sevenDaysAgo);
+    });
+    filteredOrders = dataCreatedToday;
+  } else if (showRecord === "lastMonth") {
+    const lastMonth = today.subtract(30, "days");
+    const dataCreatedToday = orders.filter((order) => {
+      return moment(order.createdAt).isSameOrAfter(lastMonth);
+    });
+    filteredOrders = dataCreatedToday;
+  } else if (showRecord === "last3Month") {
+    const last90Days = today.subtract(90, "days");
+    const dataCreatedToday = orders.filter((order) => {
+      return moment(order.createdAt).isSameOrAfter(last90Days);
+    });
+    filteredOrders = dataCreatedToday;
+  }
+
+  let filteredDeposits;
+  if (showRecord == "all") {
+    filteredDeposits = deposits;
+  } else if (showRecord === "today") {
+    const todayStart = today.startOf("day"); // Start of today
+    const dataCreatedToday = deposits.filter((dep) => {
+      return moment(dep.createdAt).isSame(todayStart, "day");
+    });
+    filteredDeposits = dataCreatedToday;
+  } else if (showRecord === "lastWeek") {
+    const sevenDaysAgo = moment().subtract(7, "days");
+    const dataCreatedToday = deposits.filter((dep) => {
+      return moment(dep.createdAt).isSameOrAfter(sevenDaysAgo);
+    });
+    filteredDeposits = dataCreatedToday;
+  } else if (showRecord === "lastMonth") {
+    const lastMonth = moment().subtract(30, "days");
+    const dataCreatedToday = deposits.filter((dep) => {
+      return moment(dep.createdAt).isSameOrAfter(lastMonth);
+    });
+    filteredDeposits = dataCreatedToday;
+  } else if (showRecord === "last3Month") {
+    const last90Days = moment().subtract(90, "days");
+    const dataCreatedToday = deposits.filter((dep) => {
+      return moment(dep.createdAt).isSameOrAfter(last90Days);
+    });
+    filteredDeposits = dataCreatedToday;
+  }
+
   return (
     <>
       <Modal
@@ -11,17 +94,60 @@ const ReportModal = ({ onClose, show, userId }) => {
         fullscreen={true}
         show={show}
         onHide={onClose}
-        className="modal-style-edit modal-style-del"
+        className="reports-modal"
         centered
       >
-        <Modal.Header
-          className="bg-transparent border-0 rounded-0 text-center p-1 pb-0 align-items-center"
-          closeButton
-        >
-          <p className="bg-transparent mb-0 w-100">Report</p>
+        <Modal.Header closeButton>
+          <h2 className="mb-0">Report</h2>
         </Modal.Header>
-        <Modal.Body className="bg-secondry d-flex flex-column gap-3 p-3 pt-0">
-          <ReportTabs userId={userId} onClose={onClose} />
+        <Modal.Body className="">
+          <Tabs activeKey={key} onSelect={(k) => setKey(k)}>
+            <Tab eventKey="tradeOperations" title="Trade operations">
+              <DataTable
+                columns={generalColumns}
+                data={fillArrayWithEmptyRows(filteredOrders, 5)}
+                customStyles={customStyle}
+                pagination
+                theme="dark"
+                paginationRowsPerPageOptions={[5, 10, 15, 20, 50]}
+              />
+            </Tab>
+            <Tab eventKey="balanceOperations" title="Balance operations">
+              <DataTable
+                columns={depositColumns}
+                data={fillArrayWithEmptyRows(filteredDeposits, 5)}
+                customStyles={customStyle}
+                pagination
+                theme="dark"
+                paginationRowsPerPageOptions={[5, 10, 15, 20, 50]}
+              />
+            </Tab>
+          </Tabs>
+          <div className="d-flex align-items-center justify-content-between mt-2">
+            <div>
+              <span className="me-2">Period</span>
+              <select
+                style={{ backgroundColor: "rgba(80,80,80,255)" }}
+                onChange={(e) => {
+                  setShowRecord(e.target.value);
+                }}
+              >
+                <option label="All Operations" value="all"></option>
+                <option label="Today" value="today"></option>
+                <option label="Last Week" value="lastWeek"></option>
+                <option label="Last Month" value="lastMonth"></option>
+                <option label="Last 3 Month" value="last3Month"></option>
+              </select>
+            </div>
+            <div className="text-center">
+              Total deals: {orders.length}
+              <br />
+              Total profit: {totalProfit && totalProfit?.toFixed(6)}
+            </div>
+            <button className=" greyBtn px-4 " onClick={onClose}>
+              Close
+            </button>
+          </div>
         </Modal.Body>
       </Modal>
     </>
