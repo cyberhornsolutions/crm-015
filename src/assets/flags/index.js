cd74da87a3e37344c93c72712f9f9c48d44a2b67
Mubasher Rasheed <bitf12m001@gmail.com>
2024-02-18 01:19:56 +0500

    limit languages

--- src/assets/flags/index.js
+++ src/assets/flags/index.js
@@ -2,11 +2,11 @@ const languages = {
   english: require("./english.png"),
   spanish: require("./spanish.png"),
   german: require("./german.png"),
-  portuguese: require("./portuguese.png"),
+  // portuguese: require("./portuguese.png"),
   russian: require("./russian.png"),
-  chinese: require("./chinese.png"),
+  // chinese: require("./chinese.png"),
   arabic: require("./arabic.png"),
-  hindi: require("./hindi.png"),
+  // hindi: require("./hindi.png"),
 };
 
 export default languages;
